f9feb6d50e437ae462590888bfd42ca15a77de07
Vayde <Vaydesb@gmail.com>
2024-11-19 14:51:47 -0600

    Section 5:46. Drawing Debug Points (Introduction of using macros, present in slash.h)

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -16,32 +16,17 @@ void AItem::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Warning, TEXT("Begin Play Called!"));
-
-	if (GEngine)
-	{
-		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Purple, FString("Item OnScreen Message!"));
-	}
-
 	UWorld* World = GetWorld();
-
 	FVector Location = GetActorLocation();
+	FVector Forward = GetActorForwardVector();
+
+	
 	DRAW_SPHERE(Location);
+	DRAW_LINE(Location, Location + Forward * 100.f);
 }
 
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
-	UE_LOG(LogTemp, Warning, TEXT("DeltaTime: %f"), DeltaTime);
-
-	if (GEngine)
-	{
-		FString Name = GetName();
-		FString Message = FString::Printf(TEXT("Item Name: %s"), *Name);
-		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Orange, Message);
-
-		UE_LOG(LogTemp, Warning, TEXT("Item Name: %s"), *Name);
-	}
 }
 
--- Source/Slash/Slash.h
+++ Source/Slash/Slash.h
@@ -5,4 +5,5 @@
 #include "CoreMinimal.h"
 
 #define DRAW_SPHERE(Location) if (GetWorld()) DrawDebugSphere(GetWorld(), Location, 25.f, 25, FColor::Red, true);
+#define DRAW_LINE(StartLocation, EndLocation) if (GetWorld()) DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, true, -1.f, 0, 1.f);
 
