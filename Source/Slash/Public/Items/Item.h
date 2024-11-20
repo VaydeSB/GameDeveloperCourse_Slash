51648c8e08fbcfe39d799f658e5d4cfb720ad517
Vayde <Vaydesb@gmail.com>
2024-11-20 15:05:29 -0600

    Section 6:59. Exposing Functions to Blueprint.

--- Source/Slash/Public/Items/Item.h
+++ Source/Slash/Public/Items/Item.h
@@ -15,7 +15,9 @@ public:
 	
 	AItem();
 	virtual void Tick(float DeltaTime) override;
-protected:	
+	
+protected:
+	
 	virtual void BeginPlay() override;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sine Parameters")
@@ -23,7 +25,14 @@ protected:
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sine Parameters")
     float TimeConstant = 5.f;
+
+	UFUNCTION(BlueprintPure)
+	float TransformedSin();
+
+	UFUNCTION(BlueprintPure)
+	float TransformedCos();
 private:
+	
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	float RunningTime;
 	
