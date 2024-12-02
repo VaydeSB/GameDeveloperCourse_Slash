43901f17d334d3c8952a46d17159a7691f2c90a3
Vayde <Vaydesb@gmail.com>
2024-12-02 09:58:10 -0600

    Section 7:65. The Pawn Class

---
+++ Source/Slash/Public/Bird.h
@@ -0,0 +1,22 @@
+// Copyright Lone Pack Studio (2024)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Bird.generated.h"
+
+UCLASS()
+class SLASH_API ABird : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	ABird();
+	virtual void Tick(float DeltaTime) override;
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+protected:
+	virtual void BeginPlay() override;
+};
